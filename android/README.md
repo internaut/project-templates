16f7412b2753cc975deb60e193d0f65c687fe668
Markus Konrad <post@mkonrad.net>
2014-12-02 11:50:58 +0100

    added basic StillImageProcNDKDroid project

---
+++ android/README.md
@@ -0,0 +1,7 @@
+# Android project templates
+
+A collection of Android project templates for quick testing of ideas or as a kick starter for smaller projects.
+
+Project | Description | IDE | Android Min/Target API Levels | Other Notes
+------- | ----------- | --- | ----------------------------- | -----------
+StillImageProcNDKDroid | Image processing for still images using the NDK | Eclipse+ADT v23 | 14 / 21 | 
---
+++ android/StillImageProcNDKDroid/src/net/mkonrad/stillimageprocndkdroid/MainActivity.java
@@ -0,0 +1,70 @@
+package net.mkonrad.stillimageprocndkdroid;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+
+public class MainActivity extends Activity {
+	private final String TAG = this.getClass().getSimpleName();
+	
+	private ImageView imgView;
+	private Bitmap origImgBm;
+	private BitmapDrawable origImgBmDr;
+	
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+		imgView = (ImageView)findViewById(R.id.img_view);
+		
+		origImgBm = BitmapFactory.decodeResource(getResources(), R.drawable.leafs_1024x786);
+		origImgBmDr = new BitmapDrawable(getResources(), origImgBm);
+		imgView.setImageDrawable(origImgBmDr);
+		
+		imgView.setOnClickListener(new ImageViewClickListener(origImgBm));
+    }
+
+	private class ImageViewClickListener implements View.OnClickListener {
+		private boolean filtered;
+		private Bitmap bitmap;
+		
+		public ImageViewClickListener(Bitmap bitmap) {
+			this.filtered = false;
+			this.bitmap = bitmap;
+		}
+		
+		@Override
+		public void onClick(View v) {
+			ImageView imgView = (ImageView)v;
+			Bitmap processedBm;
+
+			if (!filtered) {
+				processedBm = Bitmap.createBitmap(
+						bitmap.getWidth(),
+						bitmap.getHeight(),
+						Bitmap.Config.ARGB_8888);
+				
+//				filter.filter(bitmap, filteredBitmap);
+				
+				filtered = true;
+			} else {
+				processedBm = bitmap;
+				filtered = false;
+			}
+
+			BitmapDrawable filteredBitmapDrawable = new BitmapDrawable(getResources(), processedBm);
+			
+			imgView.setImageDrawable(filteredBitmapDrawable);
+		}
+		
+	}
+}
